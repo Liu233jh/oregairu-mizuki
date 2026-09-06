13b7f266a83d636b7aac9aff98cb483beadfa274
Liu233jh <2985773121@qq.com>
2026-09-06 23:24:51 +0800

    Add announcementConfig.ts - 春物欢迎公告

---
+++ src/config/announcementConfig.ts
@@ -0,0 +1,13 @@
+import type { AnnouncementConfig } from "../types/config";
+
+export const announcementConfig: AnnouncementConfig = {
+	title: "",
+	content: "欢迎来到侍奉部！本站为《我的青春恋爱物语果然有问题》主题粉丝站，收录角色介绍、剧情时间线、名台词集与高清壁纸。",
+	closable: true,
+	link: {
+		enable: true,
+		text: "了解更多",
+		url: "/about/",
+		external: false,
+	},
+};
